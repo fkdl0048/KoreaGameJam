04a3d4589760cc1bb1d2d1e2d8b8a68a7e47e194
KALI-UM <124039541+KALI-UM@users.noreply.github.com>
2023-08-12 16:34:00 +0900

    Add QTEprefab

--- Korea_GameJam/Assets/Scripts/CircleQuickTimeEvent.cs
+++ Korea_GameJam/Assets/Scripts/CircleQuickTimeEvent.cs
@@ -7,11 +7,12 @@ public class CircleQuickTimeEvent : MonoBehaviour
 {
     public float speed;
     public float checkRange;
+    public Canvas selfCanvas;
     public RectTransform arrow;
 
 
-    public Image[] circle= new Image[3];
-   
+    public Image[] circle = new Image[3];
+
 
     [SerializeField]
     private float value = 0;    //0~100
@@ -19,73 +20,107 @@ public class CircleQuickTimeEvent : MonoBehaviour
     private float angle = 0;    //0~360
     [SerializeField]
     private bool isSuccess = false;
+    [SerializeField]
+    private int checkCnt = 0;
+    [SerializeField]
+    bool panaltyTime = false;
+
+
 
     private float[] checkPoint = new float[3];
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     private void Awake()
     {
-        checkPoint[0] = Random.Range(0, 33 - checkRange);
-        checkPoint[1] = Random.Range(33, 66 - checkRange);
-        checkPoint[2] = Random.Range(66, 100 - checkRange);
-
+        SetCheckPointValue();
 
         circle[0].fillAmount = checkRange / 100f;
         circle[1].fillAmount = checkRange / 100f;
         circle[2].fillAmount = checkRange / 100f;
 
-        circle[0].GetComponent<RectTransform>().rotation = Quaternion.Euler(new Vector3(0,0, checkPoint[0] / 100*360));
-        circle[1].GetComponent<RectTransform>().rotation = Quaternion.Euler(new Vector3(0,0, checkPoint[1] / 100*360));
-        circle[2].GetComponent<RectTransform>().rotation = Quaternion.Euler(new Vector3(0,0, checkPoint[2] / 100*360));
     }
 
     // Update is called once per frame
     void Update()
-    {
-        value += speed * Time.deltaTime;
-       
-        if(value>=100)
+    {   
+        if(panaltyTime==true)
         {
-            value = value - 100;
+            return;
         }
 
-        angle = -value / 100 * 360;
-
-        arrow.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-
-
-        if(Input.GetKeyDown(KeyCode.Space))
+        //실패
+        if (value >= checkPoint[checkCnt]+checkRange*2)
         {
-            CheckQTE();
-        }
-
-
-    }
-
-
-    public bool CheckQTE()
-    {
-        if (value <= checkPoint[0] &&value>=checkPoint[0] +checkRange)
-        {
-            return true;
-        }
-        else if (value <= checkPoint[1] && value >= checkPoint[1] + checkRange)
-        {
-            return true;
-        }
-        else if (value <= checkPoint[2] && value >= checkPoint[2] + checkRange)
-        {
-            return true;
+            StartCoroutine(Penalty());
         }
         else
         {
-            return false;   
+            value += speed * Time.deltaTime;
+            angle = -value / 100 * 360;
+            arrow.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                CheckQTE();
+            }
+
         }
+    }
+    public void CheckQTE()
+    {
+        Debug.Log("now value " + value);
+        Debug.Log("now check " + checkPoint[checkCnt]);
+        if (value >= checkPoint[checkCnt] && value <= checkPoint[checkCnt] + checkRange)
+        {
+            circle[checkCnt].color = Color.green;
+            checkCnt++;
+
+            if(checkCnt==3)
+            {
+                checkCnt = 0;
+                isSuccess = true;
+                selfCanvas.gameObject.SetActive(false);
+            }
+        }
+        //실패
+        else
+        {
+            StartCoroutine(Penalty());
+        }
+    }
+
+
+    void SetCheckPointValue()
+    {
+        checkPoint[0] = Random.Range(25, 50 - checkRange);
+        checkPoint[1] = Random.Range(50, 75 - checkRange);
+        checkPoint[2] = Random.Range(75, 100 - checkRange);
+
+        circle[0].GetComponent<RectTransform>().rotation = Quaternion.Euler(new Vector3(0, 0, -checkPoint[0] / 100 * 360));
+        circle[1].GetComponent<RectTransform>().rotation = Quaternion.Euler(new Vector3(0, 0, -checkPoint[1] / 100 * 360));
+        circle[2].GetComponent<RectTransform>().rotation = Quaternion.Euler(new Vector3(0, 0, -checkPoint[2] / 100 * 360));
+
+        value = 0;    //0~100
+        angle = 0;    //0~360
+        checkCnt = 0;
+
+        circle[0].color = Color.grey;
+        circle[1].color = Color.grey;
+        circle[2].color = Color.grey;
+
+        panaltyTime = false;
+    }
+
+    private IEnumerator Penalty()
+    {
+        panaltyTime = true;
+        yield return new WaitForSeconds(1f);
+        SetCheckPointValue();
     }
 
 }
